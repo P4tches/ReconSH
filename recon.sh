ad1295a7ca3d9cbdb2668c4e87df52322365c4dd
P4tches <jonmax10@yahoo.com>
2016-12-14 16:36:53 -0800

    Add additional data

--- recon.sh
+++ recon.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
-echo "#####################"
 uname -a
-echo "#####################"
-who --count
-echo "#####################"
-netstat -nplt
-echo "#####################"
-echo "IP: $(hostname -I)"
-echo "#####################"
-tail -n 5 /var/log/auth.log
-echo "#####################"
+echo "CPU: $(top -d 0.1 -b -n2 | grep "Cpu(s)"|tail -n 1 | awk '{print $2 + $4}')% Memory: $(free | grep Mem | awk '{print $3/$2 * 100.0}')%"
+echo "IP: $(hostname -I) DNS: $(nc 8.8.8.8 53 > /dev/null && echo ok || echo error) Internet: $(echo "GET \n\n" | nc -z google.com 80 > /dev/null && echo ok || echo error)"
+date
+#echo "######################"
+#who --count
+echo "#######################"
+w | tail -n +2 -
+echo "######## Ports ########"
+netstat -nplt | tail -n +2
+echo "######## Auth #########"
+tail -n 12 /var/log/auth.log
+echo "#### Failed Logins ####"
+tail -n 5 /var/log/btmp
+echo "######################"
