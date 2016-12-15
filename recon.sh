1dce2e44e2cefe784c55da946f00e31812a8b9ed
P4tches <P4tches@users.noreply.github.com>
2016-12-15 15:18:04 -0800

    Restructured Internet Detection

--- recon.sh
+++ recon.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 uname -a
 echo "CPU: $(top -d 0.1 -b -n2 | grep "Cpu(s)"|tail -n 1 | awk '{print $2 + $4}')% Memory: $(free | grep Mem | awk '{print $3/$2 * 100.0}')%"
-echo "IP: $(hostname -I) DNS: $(nc 8.8.8.8 53 > /dev/null && echo ok || echo error) Internet: $(echo "GET \n\n" | nc -z google.com 80 > /dev/null && echo ok || echo error)"
+echo "IP: $(hostname -I) Internet: $(echo "HEAD / HTTP/1.0 \n\n" | nc -z google.com 80 > /dev/null && echo -e "\e[1;32mOK\e[0m" || echo -e "\e[1;31mERROR\e[0m") HTTP: $(echo "HEAD / HTTP/1.0 \n\n" | nc -z 173.194.219.138 80 > /dev/null && echo -e "\e[1;32mOK\e[0m" || echo -e "\e[1;31mERROR\e[0m") DNS: $(nc 8.8.8.8 53 > /dev/null && echo -e "\e[1;32mOK\e[0m" || echo -e "\e[1;31mERROR\e[0m")"
 date
 #echo "######################"
 #who --count
